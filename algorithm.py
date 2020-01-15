f3042e903014875e0322ae6e634442744abbe910
Chu Nguyên Đức <nguyenducchu1999@gmail.com>
2020-01-15 14:32:38 +0700

    Add files via upload

---
+++ algorithm.py
@@ -0,0 +1,349 @@
+import queue
+import time
+import math
+
+class Node():
+
+
+	def __init__(self, parent = None, position = None):
+		self.parent = parent
+		self.position = position
+
+		self.g = 0
+		self.h = 0
+		self.f = 0
+
+	def __eq__(self, node):
+		return self.position == node.position
+		
+	def __hash__(self):
+		return hash(self.position)
+	def __lt__(self, other): 
+		if(self.f < other.f): 
+			return True
+		else: 
+			return False
+
+def create_child_node(maze, node):
+
+	children = []
+	
+	for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (1, -1), (-1, 1), (-1, -1), (1, 1)]:
+
+		# Get child node position
+		node_position = (node.position[0] + new_position[0], node.position[1] + new_position[1])
+
+		# Make sure within range
+		if node_position[0] <= (len(maze) - 1) and node_position[0] >= 0 and node_position[1] <= (len(maze[len(maze)-1]) -1) and node_position[1] >= 0:
+
+			# Make sure walkable
+			if maze[node_position[0]][node_position[1]] == 1:
+				continue
+			'''
+			# Xét trường hợp tạo ô chéo thì không được vượt qua corner
+			if new_position	== (1, -1):
+				if maze[node.position[0]][node.position[1] - 1] == 1 and maze[node.position[0] + 1][node.position[1]] == 1:
+					continue
+			elif new_position == (-1, 1):
+				if maze[node.position[0]][node.position[1] + 1] == 1 and maze[node.position[0] - 1][node.position[1]] == 1:
+					continue
+			elif new_position == (-1, -1):
+				if maze[node.position[0]][node.position[1] - 1] == 1 and maze[node.position[0] - 1][node.position[1]] == 1:
+					continue
+					
+			else:
+				if maze[node.position[0]][node.position[1] + 1] == 1 and maze[node.position[0] + 1][node.position[1]] == 1:
+					continue
+			'''					
+
+			# Create new node
+			new_node = Node(node, node_position)
+			
+			# Tính khoảng cách f, g, h
+			#if new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+			if new_position in [(0, -1), (0, 1), (-1, 0), (1, 0)]:
+				new_node.g = node.g + 1
+			else:
+				new_node.g = node.g + 1.414
+			
+			
+			#new_node.h = ((new_node.position[0] - node.position[0]) ** 2) + ((new_node.position[1] - node.position[1]) ** 2)	#Euclide
+			#new_node.f = new_node.g + new_node.h
+
+			# Append
+			children.append(new_node)
+		
+	return children
+	
+def get_path(node):
+	path = []
+	current = node
+	while current is not None:
+		path.append(current.position)
+		current = current.parent
+
+	return path[::-1] # Return reversed path	
+	
+	
+'''	
+Hàm tính đường đi bằng thuật toán A*
+Input: 
+	- maze: Ma trận thể hiện bản đồ
+	- start, end: Vị trí bắt đầu, kết thúc (tọa độ x, y)	
+'''
+def astar(maze, start, end):
+
+	# Create start and end node
+	start_node = Node(None, start)
+	start_node.g = start_node.h = start_node.f = 0
+	end_node = Node(None, end)
+	end_node.g = end_node.h = end_node.f = 0
+
+	# Initialize open and closed list
+	open_list = []
+	closed_list = set()
+	traversal = []
+
+
+	# Add the start node
+	open_list.append(start_node)
+
+	# Loop until the end
+	while len(open_list) > 0:
+
+		# Get the current node
+		current_node = open_list[0]
+		
+		
+		index_min = 0
+		# Find node with smallest f
+		for i in range(len(open_list)):
+			if open_list[i].f < current_node.f:
+				current_node = open_list[i]
+				index_min = i
+
+		# Pop current off open list, add to closed list
+		
+		open_list.pop(index_min)
+		#traversal.append(current_node.position)
+		closed_list.add(current_node)
+
+		# Found the goal
+		if current_node == end_node:
+			return traversal, get_path(current_node)
+		
+		#Create children node
+		children = create_child_node(maze, current_node)
+		
+
+		# Loop through children
+		for child in children:
+		
+			
+			
+			#Check if child is in the closed list
+			if child in closed_list:
+					continue
+
+			child.h = math.sqrt((child.position[0] - end_node.position[0]) ** 2 + (child.position[1] - end_node.position[1]) ** 2)
+			
+			'''
+			child.h = abs(child.position[0] - end_node.position[0]) + abs(child.position[1] - end_node.position[1])
+			'''
+			child.f = child.g + child.h
+			
+			
+			# Child is already in the open list
+			appear = False
+			for open_node in open_list:
+				if child == open_node:
+					appear = True
+					if child.g < open_node.g:
+						open_node.g = child.g
+						open_node.h = child.h
+						open_node.f = child.f
+						open_node.parent = child.parent
+						
+					break
+
+			if appear == False:		
+				# Add the child to the open list
+				open_list.append(child)
+				if child.position != start_node.position and child.position != end_node.position:		
+					traversal.append(child.position)
+			
+			
+	#Open list empty but exit loop mean no path
+	return traversal, -1
+
+def bfs(maze, start, end):
+	visited = set()
+	
+	# Create start and end node
+	start_node = Node(None, start)
+	start_node.g = 0
+	end_node = Node(None, end)
+	end_node.g = 0
+	
+	queue = []
+	queue.append(start_node)
+	
+	visited.add(start_node)
+	
+	traversal = []
+	
+	while len(queue) > 0:
+	
+		current_node = queue[0]
+		queue.pop(0)
+		
+		
+
+		
+		#If end
+		if current_node.position == end:
+			return traversal, get_path(current_node)
+				
+		
+		#Create children node
+		children = create_child_node(maze, current_node)
+		
+		for child in children:
+
+			#Check if child is visited
+			if child not in visited:
+				child.g = current_node.g + 1
+				visited.add(child)
+				queue.append(child)
+				if child.position != start_node.position and child.position != end_node.position:		
+					traversal.append(child.position)
+		
+		
+	
+	return traversal, -1
+
+
+
+def ucs(maze, start, end):
+	
+	start_node = Node(None, start)
+	start_node.g = start_node.h = start_node.f = 0
+	end_node = Node(None, end)
+	end_node.g = end_node.h = end_node.f = 0
+	
+	
+	q = []
+	visited = set()
+
+	# Add the start node
+	q.append(start_node)
+	
+	traversal = []
+	
+	# Loop until the end
+	while len(q) > 0:
+		current_node = q[0]
+		
+		#Find smallest g node
+		min_index = 0
+		for i in range(len(q)):
+			if q[i].g < current_node.g:
+				current_node = q[i]
+				
+				
+				min_index = i
+			
+		visited.add(current_node)
+		q.pop(min_index)
+		
+		
+
+		#if (current_node.g, current_node.position) not in visited:
+		#	visited.add((current_node.g, current_node.position))
+		
+		# Found the goal
+		if current_node == end_node:
+			
+			return traversal, get_path(current_node)
+			
+		
+
+		#Create children node
+		children = create_child_node(maze, current_node)
+
+		# Loop through children
+		for child in children:
+			
+			
+			
+			node = Node()
+			if child in visited:
+				continue
+		
+			child.g = current_node.g + 1
+			
+			
+			# Child is already in the open list
+			appear = False
+			for node in q:
+				if child == node:
+					appear = True
+					if child.g < node.g:
+						node.g = child.g
+						node.parent = child.parent
+						node.position = child.position
+					break
+			if appear == False:
+				q.append(child)
+				if child.position != start_node.position and child.position != end_node.position:		
+					traversal.append(child.position)
+
+		
+		#time.sleep(0.5)	
+	#Open list empty but exit loop mean no path
+	
+	return traversal, -1
+	
+def dfs(maze, start, end):
+	visited = set()
+	
+	# Create start and end node
+	start_node = Node(None, start)
+	end_node = Node(None, end)
+	
+	stack = []
+	stack.append(start_node)
+	
+	
+	traversal = []
+	
+	while len(stack) > 0:
+		#current_node = q[0]
+		#q.pop(0)
+		# Get the current node
+		current_node = stack[-1]
+
+		# Pop current off open list
+		stack.pop(-1)
+		
+		if current_node not in visited:
+			visited.add(current_node)
+		#If end
+		if current_node.position == end:
+			return traversal, get_path(current_node)
+				
+		
+		#Create children node
+		children = create_child_node(maze, current_node)
+		
+		for child in children:
+		
+			
+		
+			if child not in visited:		
+				stack.append(child)	
+				if child.position != start_node.position and child.position != end_node.position:		
+					traversal.append(child.position)				
+	
+	return traversal, -1
+	
---
+++ pathfinding.py
@@ -0,0 +1,475 @@
+import tkinter as tk
+from tkinter import ttk, messagebox
+import time
+from algorithm import *
+
+#Kích thước cứng cho chương trình
+#Sửa cũng được nhưng mà phải đúng tỷ lệ
+
+HEIGHT = 600
+WIDTH = 600
+SIDE = 20 # Kích thước của 1 ô
+
+#Vậy theo kích thước này thì chương trình có 30x30 ô
+
+#Kiểm tra tại vị trí đó có phải wall
+def isWall(a, row, col):
+	return a[row][col] == 1
+
+#Kiểm tra tại vị trí đó có phải điểm bắt đầu
+def isStart(a, row, col):
+	return a[row][col] == -2
+
+#Kiểm tra tại vị trí đó có phải điểm kết thúc
+def isGoal(a, row, col):
+	return a[row][col] == 2
+
+'''
+Chương  trình có 3 class chính
+	- MainBoard 1: board chính, dùng để vẽ tường, xóa tường, ...
+	- Process: dùng để xử lý tìm đường đi, vẽ đường đi, xáo đường đi
+	- OptionBoard: có các button chạy, xóa, chọn thuật toán, 
+'''
+
+	
+#Board chính
+class MainBoard(tk.Frame): #Kế thừa frame của tkinter
+	def __init__(self, root, h = HEIGHT, w=WIDTH):
+		
+		super().__init__(root, width=h, height=w)
+
+		
+		self.canvas = tk.Canvas(self, width = WIDTH, height=HEIGHT, bg='white')
+		
+		heightMatrix = h//SIDE
+		widthMatrix = w//SIDE
+
+		self.a = [[0] * (heightMatrix) for i in range(widthMatrix)] #Ma trận của Board
+		
+		
+		#0 là đi được, 1 là không đi được
+		
+		#Khởi tạo điểm bắt đầu và kết thúc
+		self.startNode = Node(None, (heightMatrix//2, 0))
+		self.endNode = Node(None, (heightMatrix//2, widthMatrix-1))
+		
+		#Set ma trận cho start, end
+		startRow, startCol = self.startNode.position
+		endRow, endCol = self.endNode.position
+		
+		
+		self.a[startRow][startCol] = -2 #Điểm bắt đầu có giá trị -2
+		self.a[endRow][endCol] = 2 #Điểm kết thúc có giá trị 2
+		
+		#List chứa những vị trí là wall
+		self.wallList = []
+		
+		
+		
+	def drawBoard(self):
+	
+		self.pack()
+		self.canvas.pack()
+		#Vẽ các đường ngang dọc để tạo thành bàn cờ
+		
+		#Vẽ đường ngang
+		x1 = 0
+		x2 = WIDTH
+		for k in range(0, HEIGHT, SIDE):
+			y1 = k
+			y2 = k
+			self.canvas.create_line(x1, y1, x2, y2)
+		#Vẽ đường dọc
+		y1 = 0
+		y2 = HEIGHT
+		for k in range(0, WIDTH, SIDE):
+			x1 = k
+			x2 = k
+			self.canvas.create_line(x1, y1, x2, y2)
+			
+		#Tô màu điểm bắt đầu và kết thúc	
+		self.highlight(self.startNode.position[0], self.startNode.position[1], 'green')
+		self.highlight(self.endNode.position[0], self.endNode.position[1], 'orange')
+		
+		
+	#Tạo thao tác với người dùng	
+	def setUI(self):
+		
+		# Click event handling
+		self.canvas.bind("<Button-1>", self.callbackClick)
+
+
+	#Xoa thao tac voi nguoi dung	
+	def disableUI(self):
+		
+		self.canvas.unbind("<Button-1>")
+		self.canvas.unbind("<B1-Motion>")
+
+	#Update điểm bắt đầu và kết thúc khi được thao tác
+	def updateStart(self, row, col):
+		self.a[row][col] = -2
+		self.startNode.position = (row, col)
+		
+	def updateGoal(self, row, col):
+		self.a[row][col] = 2
+		self.endNode.position = (row, col)
+	
+	
+	#Tô màu 1 ô được click vào
+	def highlight(self, row, col, color):
+		
+		#Thực chất là tạo hình chữ nhật màu khác ở vị trí đó
+		x0 = col * SIDE
+		y0 = row * SIDE
+
+		x1 = (col + 1) * SIDE
+		y1 = (row + 1) * SIDE
+		
+		#Nếu màu xám thì set value và thêm vị trí vào wallList
+		if color == 'darkslategray':
+			self.a[row][col] = 1
+			self.wallList.append((row, col))
+			
+		#Nếu màu xanh lá thì updateStart
+		elif color == 'green':
+			self.updateStart(row, col)
+			
+		#Nếu màu cam lá thì updateGoal	
+		elif color == 'orange':
+			self.updateGoal(row, col)
+			
+		self.canvas.create_rectangle(x0, y0, x1, y1, fill=color)
+
+		
+	#Xóa màu 1 ô
+	def hide(self, row, col):
+		
+		#Nếu là tường thì xóa vị trí trong wallList
+		if self.a[row][col] == 1:
+			self.wallList.remove((row, col))
+			
+				
+		#Set value 
+		self.a[row][col] = 0
+		
+		#Thực chất là tạo hình chữ nhật màu trắng ở vị trí đó
+		x0 = col * SIDE
+		y0 = row * SIDE
+
+		x1 = (col + 1) * SIDE
+		y1 = (row + 1) * SIDE
+
+		self.canvas.create_rectangle(x0, y0, x1, y1, fill='white')
+				
+	#Hàm thực hiện vẽ tường, xóa tường, ... khi người dùng thao tác
+	def callbackClick(self, event):
+	
+
+		self.canvas.focus_set()
+		
+		#Lấy giá trị dòng và cột được click
+		row, col = int((event.y) / SIDE), int((event.x) / SIDE)
+		
+		
+		#Nếu click vào điểm bắt đầu hoặc kết thúc thì hold sẽ là kéo điểm
+		if isStart(self.a, row, col):
+			self.canvas.bind("<B1-Motion>", self.dragStart)
+		
+		elif isGoal(self.a, row, col):
+			self.canvas.bind("<B1-Motion>", self.dragGoal)
+		
+		else:
+		
+			#Neu click vao wall thi hold sẽ là xóa tường
+			if isWall(self.a, row, col):
+				self.hide(row, col)
+				self.canvas.bind("<B1-Motion>", self.deleteWall)
+				
+			#Neu click ô rỗng thi hold se la tạo tường	
+			else:
+				self.highlight(row, col, 'darkslategray')
+				self.canvas.bind("<B1-Motion>", self.createWall)
+				
+		#NOTE: Cảm thấy hàm này hơi rườm rà, chắc là có thể làm mượt hơn
+		
+	#Kéo điểm bắt đầu	
+	def dragStart(self, event):
+	
+		self.canvas.focus_set()
+		
+		#Điểm cũ
+		oldRow = self.startNode.position[0]
+		oldCol = self.startNode.position[1]
+		
+		#Điểm mới
+		row, col = int((event.y) / SIDE), int((event.x) / SIDE)
+		
+		#Nếu đủ điều kiện thì thực hiện kéo điểm bằng cách tô điểm mới và xóa điểm cũ
+		if not isWall(self.a, row, col) and (oldRow, oldCol) != (row, col) and not isGoal(self.a, row, col):
+			self.highlight(row, col, 'green')
+			self.hide(oldRow, oldCol)
+	
+	#Kéo điểm kết thúc	
+	def dragGoal(self, event):
+	
+		self.canvas.focus_set()
+		
+		#Điểm cũ
+		oldRow = self.endNode.position[0]
+		oldCol = self.endNode.position[1]
+		
+		#Điểm mới
+		row, col = int((event.y) / SIDE), int((event.x) / SIDE)
+		
+		#Nếu đủ điều kiện thì thực hiện kéo điểm bằng cách tô điểm mới và xóa điểm cũ
+		if not isWall(self.a, row, col) and (oldRow, oldCol) != (row, col) and not isStart(self.a, row, col):
+			self.highlight(row, col, 'orange')
+			self.hide(oldRow, oldCol)
+		
+	#NOTE: Cảm thấy là 2 hàm trên có thể gộp
+	
+
+	#Tạo wall
+	def createWall(self, event):
+
+		self.canvas.focus_set()
+		
+		#Lấy giá trị dòng và cột được click
+		row, col = int((event.y) / SIDE), int((event.x) / SIDE)
+		
+		#Nếu đủ điều kiện thì tô màu ô đó
+		if not isWall(self.a, row, col) and not isStart(self.a, row, col) and not isGoal(self.a, row, col):
+			self.highlight(row, col, 'darkslategray')
+			
+	
+	#Xóa wall
+	def deleteWall(self, event):
+
+		self.canvas.focus_set()
+		
+		#Lấy giá trị dòng và cột được click
+		row, col = int((event.y) / SIDE), int((event.x) / SIDE)
+		
+		#Nếu điểm là wall, xóa ô đó
+		if isWall(self.a, row, col):
+			self.hide(row, col)
+	
+	
+class Process(MainBoard):
+	def __init__(self, root):
+		
+		
+		
+		super().__init__(root)
+		
+		
+		
+		#Thứ tự duyệt đường đi
+		self.traversal = []
+		
+		
+		#Đường đi
+		self.path = []
+		
+		#Thuật toán sử dụng, default là A*
+		self.algo = "A* Search"
+		
+	
+	#Hàm xử lý khi click Run
+	def findPath(self):
+		
+		#Xóa đường đi cũ trước khi vẽ đường đi mới
+		self.clearPath()
+		
+		#Biến tạm
+		traversal, path = 0, 0
+		
+		
+		#Choosing algorithm
+		if self.algo == "A* Search":
+		
+			traversal, path = astar(self.a, self.startNode.position, self.endNode.position)
+		
+		elif self.algo == "Dijkstra Search":
+			traversal, path = ucs(self.a, self.startNode.position, self.endNode.position)
+			
+		elif self.algo == "Breadth First Search" :
+			traversal, path = bfs(self.a, self.startNode.position, self.endNode.position)
+			
+		else:
+			traversal, path = dfs(self.a, self.startNode.position, self.endNode.position)
+		
+		#NOTE: Khúc trên rườm rà quá, có thể làm gọn
+		
+		#Không tìm được đường đi xuất thông báo
+		if path == -1:
+			messagebox.showerror(message='No path', title='Error')
+		
+		else:
+			self.traversal = traversal
+			self.path = path
+			
+			#Vẽ đường đi
+			self.drawSearch()
+
+		
+	#Hàm xóa tất cả khi click Clear All		
+	def clearAll(self):
+
+		#Xóa wall
+		#Xóa phần tử đầu cho đến khi rỗng
+		while self.wallList != []:
+			self.hide(self.wallList[0][0], self.wallList[0][1])
+			
+		#Xóa đường đi
+		self.clearPath()
+		
+	#Xóa đường đi	
+	def clearPath(self):
+	
+		#Xóa phần tử đầu cho đến khi rỗng	
+		while self.path != []:
+			
+			if self.path[0] != self.startNode.position and self.path[0] != self.endNode.position and not isWall(self.a, self.path[0][0], self.path[0][1]):
+				self.hide(self.path[0][0], self.path[0][1])
+			self.path.pop(0)
+		
+		while self.traversal != []:
+			if self.traversal[0] != self.startNode.position and self.traversal[0] != self.endNode.position and not isWall(self.a, self.traversal[0][0], self.traversal[0][1]):
+				self.hide(self.traversal[0][0], self.traversal[0][1])
+			self.traversal.pop(0)
+			
+	#NOTE: Hàm trên quá dài đi
+
+	#Hàm vẽ đường đi
+	def drawSearch(self, i=0, j=1):
+		#Trong quá trình vẽ thì không được thao tác 
+		'''
+		self.runButton.config(state = "disable")	#Liệt nút Run
+		self.clearButton.config(state = "disable")	#Liệt nút Clear All
+		'''
+		
+		
+		self.disableUI()							#Liệt thao tác Board chính
+	
+		#Khúc này khó quá quên rồi
+		if i >= len(self.traversal)-1:
+			if j >= len(self.path)-1:
+				#Khúc này là kết thúc vẽ
+				'''
+				self.runButton.config(state = "active")		#Làm nút Run bấm được
+				self.clearButton.config(state = "active")	#Làm nút ClearAll bấm được
+				'''
+				self.setUI()								#Làm Board chính thao tác được
+				return
+			else:
+				self.highlight(self.path[j][0], self.path[j][1], 'yellow')
+				self.canvas.after(25, lambda: self.drawSearch(i, j+1))
+			#return
+		else:
+			self.highlight(self.traversal[i][0], self.traversal[i][1], 'deepskyblue')
+			self.canvas.after(5, lambda: self.drawSearch(i+1))
+			
+		
+			
+	
+	
+#Board process
+class OptionBoard(tk.Frame):
+	def __init__(self, root, h, w, process):
+		'''
+		#Main Board
+		self.board = board
+		'''
+		
+		#Frame bên cạnh Main Board
+		super().__init__(root, width=h, height=w)
+		self.pack(side='left', fill='both')
+		
+		
+		self.process = process
+		
+		
+		#Button Run
+		self.runButton = tk.Button(self, text='Run', width = 10, command = self.process.findPath)
+	
+		#Button Clear
+		self.clearButton = tk.Button(self, text='Clear All', width = 10, command = self.process.clearAll) 
+		
+		
+		#Button Clear Path
+		self.clearPathButton = tk.Button(self, text='Clear Path', width = 10, command=self.process.clearPath) 
+		
+		#Combobox chọn thuật toán
+		self.algoBox = ttk.Combobox(self, 
+                            values=[
+                                    "A* Search", 
+                                    "Dijkstra Search",
+                                    "Breadth First Search",
+                                    "Depth First Search"])
+		
+		self.algoBox.current(0)
+		self.algoBox.bind("<<ComboboxSelected>>", self.chooseAlgo)
+		
+		
+		
+	
+	def drawOptionBoard(self):
+		self.pack()
+		self.runButton.pack()
+		self.clearButton.pack()
+		self.clearPathButton.pack()
+		self.algoBox.pack()
+		
+	
+	#Hàm set thuật toán khi được click chọn thuật toán
+	def chooseAlgo(self, event):
+			self.process.algo = self.algoBox.get()
+	
+	
+
+
+def main():
+
+
+	root = tk.Tk()
+	root.title("Pathfinding Visualizer")
+	'''
+	
+	frame = tk.Frame(root, width=WIDTH+20, height=HEIGHT)
+	frame.pack()
+	'''
+	'''
+	frame = tk.Frame(root, width=WIDTH, height=HEIGHT)
+	frame.pack(side='left')
+	'''
+	'''
+	main_board = MainBoard(root)
+	main_board.drawBoard()
+	main_board.setUI()
+	'''
+	
+	
+	process = Process(root)
+	process.drawBoard()
+	process.setUI()
+	
+	
+	option_board = OptionBoard(root, 600, 200, process)
+	option_board.drawOptionBoard()
+	
+	
+	root.mainloop()
+	
+main()
+	
+
+
+
+
+
+
+
+
+
+
